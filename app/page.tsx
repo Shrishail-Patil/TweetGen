fbadc2cfef72ab7de26bd1b7e0e68ee25fd202f5
ShrishailPatil <apple@Shrishails-MacBook-Air.local>
2025-02-08 20:26:45 +0530

    analytics added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import TweetGenerator from "@/app/components/TweetGenerator"
 import FloatingElements from "./components/FloatingElements"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-8 relative overflow-hidden">
+      <Analytics/>
       <FloatingElements />
       <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 relative z-10">SaaS Tweet Generator</h1>
       <TweetGenerator />
